894efa66f9e5317c798611fe99a2de8031dc507c
huming@exp-corp.com <huming@exp-corp.com>
2020-03-26 23:44:24 +0800

    测试版上传

---
+++ precache-manifest.466b5af2d93467ef58a4152b36f07226.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d678e807988706246fdd27b56ec9d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bde3b3f21cf752822f7",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "6af9e34ddd488e5734f6",
+    "url": "/static/css/main.ecbd5c54.chunk.css"
+  },
+  {
+    "revision": "6bde3b3f21cf752822f7",
+    "url": "/static/js/2.84b797d1.chunk.js"
+  },
+  {
+    "revision": "6af9e34ddd488e5734f6",
+    "url": "/static/js/main.a3406976.chunk.js"
+  },
+  {
+    "revision": "b1e92f579ddd9024ec72",
+    "url": "/static/js/runtime~main.a80e8a21.js"
+  },
+  {
+    "revision": "4d2f2f4baa617ba08685b4d9118dc029",
+    "url": "/static/media/bg-pricing.4d2f2f4b.jpeg"
+  },
+  {
+    "revision": "6ee66b2cb8a3d9988e37b83b3b4a590e",
+    "url": "/static/media/card-1.6ee66b2c.jpeg"
+  },
+  {
+    "revision": "ae10829f019b0103525a6cd1102ee213",
+    "url": "/static/media/card-2.ae10829f.jpeg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "fa83cdfdf16735504ebc861e693dbba4",
+    "url": "/static/media/dimpact_logo.fa83cdfd.jpeg"
+  },
+  {
+    "revision": "35f7d126c07a40a893f3f6cc8d1c3717",
+    "url": "/static/media/lock.35f7d126.jpeg"
+  },
+  {
+    "revision": "59fc83e26c663ade5dcae6e7d56ca970",
+    "url": "/static/media/login.59fc83e2.jpeg"
+  },
+  {
+    "revision": "30b3d12d4b1732849444f8d254e87645",
+    "url": "/static/media/logo.30b3d12d.png"
+  },
+  {
+    "revision": "49b54380ba092ba68ab160f3dd8d5773",
+    "url": "/static/media/register.49b54380.jpeg"
+  },
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/static/media/sidebar-2.d30c9e30.jpg"
+  }
+]);
